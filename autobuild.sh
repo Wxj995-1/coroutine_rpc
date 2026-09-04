dfb3b649bbb60dba42e950ad863facc17e5143a4
Wxj995-1 <za00011@163.com>
2026-09-04 19:00:02 +0800

    feat:完整自动化编译脚本

--- autobuild.sh
+++ autobuild.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
-
 set -e
 
-cmake -S . -B build
-cmake --build build -j
+BUILD_DIR="build"
+
+# 第一个参数处理
+case "${1:-}" in
+  clean)
+    rm -rf "$BUILD_DIR"
+    exit 0
+    ;;
+  debug|Debug|DEBUG)
+    BUILD_TYPE="Debug"
+    ;;
+  release|Release|RELEASE)
+    BUILD_TYPE="Release"
+    ;;
+  *)
+    BUILD_TYPE="Debug"
+    ;;
+esac
+
+mkdir -p "$BUILD_DIR"
+cd "$BUILD_DIR"
+cmake .. -DCMAKE_BUILD_TYPE="$BUILD_TYPE"
+cmake --build . -- -j"$(nproc)"
